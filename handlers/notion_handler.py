e586ab4c78734a3b6da5678bc04f03c25b04e093
Kostachy <kostachy@gmail.com>
2023-12-09 22:36:24 +0300

    add write to notion db func

--- handlers/notion_handler.py
+++ handlers/notion_handler.py
@@ -1,3 +1,5 @@
+import logging
+
 from aiogram import Router, F
 from aiogram.filters import CommandStart
 from aiogram.types import Message
@@ -5,9 +7,21 @@ from aiogram.types import Message
 router = Router()
 
 
+data = {}  # потом заменю на полноценную бд
+
+
 @router.message(CommandStart())
 async def get_start(message: Message):
-    await message.answer('...')
+    await message.answer('Введите категории ваших задач через пробел')
+
+
+@router.message(F.text)
+async def get_tasks(message: Message):
+    global data
+    data['tasks'] = ['Task_Name', ]
+    logging.info(data)
+    await message.answer("👍")
+
 
 
 
--- notion/notion_api.py
+++ notion/notion_api.py
@@ -6,6 +6,8 @@ import json
 DB_ID = "ff715c8bdd53406e826a6f0f8d9af46a"
 
 
+# https://www.notion.so/6aea1e4874e34ea1bf16bf0824d95a06?v=146ee4d81298452fbddd8f6ac920ba8c&pvs=4
+# https://www.notion.so/ff715c8bdd53406e826a6f0f8d9af46a?v=5009098538b54e679486c032db69025a&pvs=4
 def write_dict_to_file_as_json(content, file_name):
     content_as_json_str = json.dumps(content)
 
@@ -30,14 +32,12 @@ def safe_get(data, dot_chained_keys):
     return data
 
 
-def main():
-    client = Client(auth="secret_eOEd2ENynh5mQ1ztBDR0G6NIcUId9qxSyJPM0nytukH")
+def read_db(client: Client):
     db_info = client.databases.retrieve(database_id=DB_ID)
     write_dict_to_file_as_json(db_info, 'db_info.json')
 
     db_rows = client.databases.query(database_id=DB_ID)
     write_dict_to_file_as_json(db_rows, 'db_rows.json')
-    pprint(db_rows)
 
     simple_rows = []
     for row in db_rows['results']:
@@ -47,16 +47,31 @@ def main():
         tags = safe_get(row, 'properties.Tags.multi_select.0.name')
 
         simple_rows.append({
-            'task_name': task_name,
-            'tags': tags,
+            'Task_Name': task_name,
+            'Tags': tags,
             'start_date': start_date,
             'end_date': end_date
         })
 
-        for i in simple_rows:
-            pprint(i)
-
         write_dict_to_file_as_json(simple_rows, 'simple_rows.json')
 
+
+def write_row(client: Client, database_id, task_name, tags, start_date, end_date):
+    client.pages.create(
+        **{
+            'parent': {
+                'database_id': database_id
+            },
+            'properties': {
+                'Task_Name': {'title': [{'text': {'content': task_name}}]},
+                'Tags': {'multi_select': {'name': tags}},
+                'Date': {'date': {'start': start_date, 'end': end_date}}
+            }
+        }
+    )
+
+
 if __name__ == "__main__":
-    main()
+    client = Client(auth="secret_eOEd2ENynh5mQ1ztBDR0G6NIcUId9qxSyJPM0nytukH")
+    read_db(client)
+    write_row(client, DB_ID, 'Nameee', 'code', '2023-12-09', '2023-12-11')
