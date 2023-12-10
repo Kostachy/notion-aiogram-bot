e97a61fd8da718aabd5426727fc5f716d6c7b9f1
Kostachy <kostachy@gmail.com>
2023-12-11 00:27:49 +0300

    start to add openai features

--- openai_api/api.py
+++ openai_api/api.py
@@ -1,34 +1,22 @@
-from openai import OpenAI
+import asyncio
+
+from openai import AsyncOpenAI
+from httpx import AsyncClient
+
+http_cliet = AsyncClient(proxies="http://51.79.229.202:3128")
+client = AsyncOpenAI(api_key="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", http_client=http_cliet)
 
 
-client = OpenAI(api_key=...)
+async def create_assistant():
+    assistant = await client.beta.assistants.create(
+        name="Task Assistant",
+        instructions="",
+        tools=[{"type": "function"}],
+        model="gpt-4-1106-preview",
+    )
+    return assistant
 
-assistant = client.beta.assistants.create(
-    name="Math Tutor",
-    instructions="You are a personal math tutor. Write and run code to answer math questions.",
-    tools=[{"type": "code_interpreter"}],
-    model="gpt-3.5-turbo"
-)
 
-thread = client.beta.threads.create()
 
-message = client.beta.threads.messages.create(
-    thread_id=thread.id,
-    role="user",
-    content="I need to solve the equation `3x + 11 = 14`. Can you help me?"
-)
-
-run = client.beta.threads.runs.create(
-  thread_id=thread.id,
-  assistant_id=assistant.id,
-  instructions="Please address the user as Jane Doe. The user has a premium account."
-)
-
-run = client.beta.threads.runs.retrieve(
-  thread_id=thread.id,
-  run_id=run.id
-)
-
-messages = client.beta.threads.messages.list(
-  thread_id=thread.id
-)
+if __name__ == "__main__":
+    asyncio.run(create_assistant())
