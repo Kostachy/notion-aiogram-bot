d7220e2013569102211b04a1af2abbbb5b1c44f8
Kostachy <kostachy@gmail.com>
2023-12-11 00:27:01 +0300

    add pools

--- db/database.py
+++ db/database.py
@@ -1,9 +1,16 @@
+from sqlalchemy import AsyncAdaptedQueuePool
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
 from config import settings
 
-engine = create_async_engine(settings.db_url, echo=True)
+engine = create_async_engine(settings.db_url,
+                             echo=True,
+                             poolclass=AsyncAdaptedQueuePool,
+                             pool_size=12,
+                             max_overflow=4,
+                             pool_pre_ping=True
+                             )
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
