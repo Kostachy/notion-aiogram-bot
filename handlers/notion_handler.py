795e7b80511d9919033e5984c4aa033a4a8384ea
Kostachy <kostachy@gmail.com>
2023-12-13 22:33:59 +0300

    trying to integrate assistant

--- handlers/notion_handler.py
+++ handlers/notion_handler.py
@@ -1,9 +1,11 @@
+import asyncio
 import logging
 
 from aiogram import Router, F
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
+from config import settings
 from db.crud.user_crud import UserCrud
 from utils import get_notion_db_id
 from notion.notion_api import notion_client
@@ -35,9 +37,53 @@ async def get_notion_db_link_and_tasks(message: Message):
 @router.message(F.text)
 async def get_opneai_help(message: Message):
     thread_id = await UserCrud.get_thread_id(user_id=message.from_user.id)
-    print(thread_id)
-    await openai_client.add_message_to_thread(thread=thread_id, message_id=message.text)
-    await message.answer("")
+    await openai_client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=message.text
+    )
+    run = await openai_client.beta.threads.runs.create(
+        thread_id=thread_id,
+        assistant_id=settings.ASSISTANT_ID
+    )
+    while run.status not in ["completed", "failed", "requires_action"]:
+        run = await openai_client.beta.threads.runs.retrieve(
+            thread_id=thread_id,
+            run_id=run.id
+        )
+        await asyncio.sleep(3)
+        await message.reply(run.status)
+    tools_to_call = run.required_action.submit_tool_outputs.tool_calls
+    tools_output_array = []
+    for tool in tools_to_call:
+        tool_call_id = tool.id
+        function_name = tool.function.name
+
+        output = False
+        if function_name == "insert_task_and_time":
+            output = True
+
+        tools_output_array.append({"tool_call_id": tool_call_id, "output": output})
+
+    run = await openai_client.beta.threads.runs.submit_tool_outputs(
+        thread_id=thread_id,
+        run_id=run.id,
+        tool_outputs=tools_output_array
+    )
+
+    while run.status not in ["completed", "failed", "requires_action"]:
+        run = await openai_client.beta.threads.runs.retrieve(
+            thread_id=thread_id,
+            run_id=run.id
+        )
+        await message.reply(run.status)
+
+    messages = openai_client.beta.threads.messages.list(
+        thread_id=thread_id
+    )
+
+    for text in messages:
+        await message.answer(text.content[0].text.value)
 
 
 @router.message()
